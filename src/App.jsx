d3ee4fd17afd52c997df66db514da868dc3b8518
Matthew Torma <tormamatthew@gmail.com>
2024-11-07 19:09:38 -0600

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
-import MainPage from './Components/MainPage'
+import BookDetails from './Components/BookDetailsPage';
+import BookList from './Components/BookListPage'
+// import MainPage from './Components/MainPage'
 import NavBar from './Components/NavBar'
+import { useState, useEffect } from "react";
+const App = () => {
+  const [allbooks, setBookList] = useState(true);
+  const [bookdetail, setBookDetails] = useState({});
 
-function App() {
-  
 
   return (
     <>
-      <NavBar/>
-      <MainPage/>
+      <NavBar />
+      {
+        allbooks ?
+        <BookList
+          setBookList={setBookList}
+          setBookDetails={setBookDetails} />:
+        <BookDetails
+          bookdetail={bookdetail}
+          setBookList={setBookList}
+        />
+      }
     </>
   )
 }
--- src/Components/BookListPage.jsx
+++ src/Components/BookListPage.jsx
@@ -1,10 +1,35 @@
-import { useState. useEffect } from "react";
-const BookList = () => {
-  const [bookList, setBookList] = useState();
-  
-  const useEffect = () = {
-    
+import {useState, useEffect} from "react";
+const BookList = ({setBookList, setBookDetails }) => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`);
+      const result = await response.json();
+      setBooks(result.books);
+      console.log(result.books)
+    }
+    getBooks();
+  }, []);
+  const clickedUser = (book) => {
+    setBookList(false);
+    setBookDetails(book);
   }
-}
+  return (
+    <>
+      <ol>
+        {
+          books.map((singleBook) => {
+            return <li
+              key={singleBook.id}
+              onClick={() => clickedUser(singleBook)}
+            >
+              {singleBook.title}
+            </li>
+          })
+        }
+      </ol>
+    </>
+  )
+};
 
 export default BookList
